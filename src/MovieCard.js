fe1c24f821db472d4ec9aa93d10486602ec4964d
Akshay Oza <lakshayoza126@gmail.com>
2022-05-30 19:37:24 +0530

    React Movie Application

---
+++ src/MovieCard.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+export default function MovieCard({movie:{Year,Poster,Title,Type,imdbID}}) {
+  return (
+    <div className="movie" key={imdbID}>
+      <div>
+        <p>{Year}</p>
+      </div>
+      <div>
+        <img
+          src={
+            Poster !== "N/A"
+              ? Poster
+              : "https://via.placeholder.com/400"
+          }
+          alt={Title}
+        />
+      </div>
+      <div>
+        <span>{Type}</span>
+        <h3>{Title}</h3>
+      </div>
+    </div>
+  );
+}
